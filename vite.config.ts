6825fd90d1e2a0939bd56c7b17c5613c7e4785e6
Artem Bondarchuk <arhipilagys@gmail.com>
2025-09-03 16:13:08 +1100

    trying to fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/kata-vegetables-shop/',
   test: {
     globals: true,
     environment: 'jsdom',
